a6123afae84f043a1dc96bb4e44aeed45b44861c
RicardoTrevino <ricardotrevino24@gmail.com>
2016-04-12 15:25:14 -0500

    Second commit

---
+++ AppVidaSaludable/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AppVidaSaludable
+//
+//  Created by RicardoTrevino on 4/12/16.
+//  Copyright © 2016 RicardoTrevino. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
