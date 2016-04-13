760f502b8a9730d57a6a67a685530c2a5b7800b8
RicardoTrevino <ricardotrevino24@gmail.com>
2016-04-13 16:04:06 -0500

    Agrega la opción de seleccionar los días, falta el botón save

--- AppVidaSaludable/TableViewControllerDias.swift
+++ AppVidaSaludable/TableViewControllerDias.swift
@@ -1,5 +1,5 @@
 //
-//  TableViewControllerAgregar.swift
+//  TableViewControllerDias.swift
 //  AppVidaSaludable
 //
 //  Created by RicardoTrevino on 4/13/16.
@@ -8,28 +8,11 @@
 
 import UIKit
 
-class TableViewControllerAgregar: UITableViewController,UIPickerViewDataSource, UIPickerViewDelegate {
-    
+class TableViewControllerDias: UITableViewController {
 
-    // MARK: - Outlets
-    
-    @IBOutlet weak var pickerViewHora: UIPickerView!
-    @IBOutlet weak var pickerViewCategoria: UIPickerView!
-    
-    // MARK: - Variables
-    var arreglo: [[AnyObject]] = []
-    var arreglo1 = ["1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23"]
-    var arreglo2 = ["1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23", "24","25", "26", "27", "28", "29", "30", "31","32", "33", "34", "35", "36","37", "38", "39", "40", "41", "42", "43","44", "45", "46", "47", "48","49", "50", "51", "52", "53", "54", "55","56", "57", "58", "59"]
-    var arregloCategorias: [String] = ["Alimentación", "Hidratación", "Actividad Física", "Actividad Social"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        arreglo = [arreglo1, arreglo2]
-        self.pickerViewHora.delegate = self
-        self.pickerViewHora.dataSource = self
-       
-        self.pickerViewCategoria.delegate = self
-        self.pickerViewCategoria.dataSource = self
+        self.tableView.allowsMultipleSelection = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -37,31 +20,6 @@ class TableViewControllerAgregar: UITableViewController,UIPickerViewDataSource, 
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    // MARK: - Picker
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        if pickerView == pickerViewHora {
-            return arreglo.count
-        }else{
-            return 1
-        }
-    }
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == pickerViewHora {
-            return arreglo[component].count
-        }else{
-            return arregloCategorias.count
-        }
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == pickerViewHora {
-        return arreglo[component][row] as? String
-        }else{
-            return arregloCategorias[row] as String
-        }
-    }
-  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -76,7 +34,15 @@ class TableViewControllerAgregar: UITableViewController,UIPickerViewDataSource, 
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 4
+        return 7
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.Checkmark
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.None
     }
 
     /*
