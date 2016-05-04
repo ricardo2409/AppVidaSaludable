184ccfd795a8d3dc437e784ad83a7b2d4a13f116
RicardoTrevino <ricardotrevino24@gmail.com>
2016-05-04 16:25:41 -0500

    Minutos = 0, 15, 30, 45

--- AppVidaSaludable/TableViewControllerAgregar.swift
+++ AppVidaSaludable/TableViewControllerAgregar.swift
@@ -17,14 +17,13 @@ class TableViewControllerAgregar: UITableViewController,UIPickerViewDataSource, 
     @IBOutlet weak var tfNombre: UITextField!
     @IBOutlet weak var pickerViewHora: UIPickerView!
     @IBOutlet weak var pickerViewCategoria: UIPickerView!
-    
     @IBOutlet weak var botonCancel: UIBarButtonItem!
     @IBOutlet weak var botonGuardar: UIBarButtonItem!
     @IBOutlet weak var lbFrecuencia: UILabel!
     // MARK: - Variables
     var arreglo: [[AnyObject]] = []
     var arreglo1 = ["1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23"]
-    var arreglo2 = ["0","1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23", "24","25", "26", "27", "28", "29", "30", "31","32", "33", "34", "35", "36","37", "38", "39", "40", "41", "42", "43","44", "45", "46", "47", "48","49", "50", "51", "52", "53", "54", "55","56", "57", "58", "59"]
+    var arreglo2 = ["0","15","30","45"]
     var arregloCategorias: [String] = ["Alimentación", "Hidratación", "Actividad Física", "Actividad Social"]
     var arregloDias: [Int] = []
     var categoria : String!
--- TableViewControllerEditarActividad.swift
+++ TableViewControllerEditarActividad.swift
@@ -31,7 +31,7 @@ class TableViewControllerEditarActividad: UITableViewController,UIPickerViewData
     let Act = Actividades()
 
     var arreglo1 = ["1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23"]
-    var arreglo2 = ["1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23", "24","25", "26", "27", "28", "29", "30", "31","32", "33", "34", "35", "36","37", "38", "39", "40", "41", "42", "43","44", "45", "46", "47", "48","49", "50", "51", "52", "53", "54", "55","56", "57", "58", "59"]
+    var arreglo2 = ["0","15","30","45"]
     var arregloCategorias: [String] = ["Alimentación", "Hidratación", "Actividad Física", "Actividad Social"]
     var arregloDias: [Int] = []
    
