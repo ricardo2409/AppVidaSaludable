6f6a6fd0fea862633834c456486679046bed9ff8
RicardoTrevino <ricardotrevino24@gmail.com>
2016-05-04 22:22:35 -0500

    Background Fetch funciona ;)

--- AppVidaSaludable/TableViewControllerActividades.swift
+++ AppVidaSaludable/TableViewControllerActividades.swift
@@ -174,7 +174,8 @@ class TableViewControllerActividades: UITableViewController {
                 notification.soundName = UILocalNotificationDefaultSoundName
                 notification.alertBody = arregloActividadesHoy[i].nombre
                 notification.alertTitle = arregloActividadesHoy[i].categoria
-                notification.repeatInterval = .Weekly
+                notification.repeatInterval = NSCalendarUnit.Weekday
+
                 notification.fireDate = date
                 UIApplication.sharedApplication().scheduleLocalNotification(notification)
                 print("Esta es la notificacion que creé ")
